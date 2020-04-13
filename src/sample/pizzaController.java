f22473a9ba0b1a39250cf740c8090f868bd856d2
fareselkhouli <fareselkhouli@gmail.com>
2020-04-13 02:29:31 -0400

    implemented show Order button

---
+++ src/sample/pizzaController.java
@@ -0,0 +1,117 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import sample.pizza.*;
+import sample.scene2.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+
+public class pizzaController implements Initializable{
+
+    ObservableList<String> toppings = FXCollections.observableArrayList(
+            "Beef", "Cheese", "Chicken", "Green Pepper", "Ham",
+            "Mushroom", "Onion", "Pepperoni", "Pineapple", "Sausage");
+    ObservableList<String> types = FXCollections.observableArrayList("Build your own",
+            "Deluxe",
+            "Hawaiian");
+    ObservableList<String> sizes = FXCollections.observableArrayList("Medium",
+            "Small",
+            "Large");
+    Image byoImage = new Image("file:buildyourown.jpg");
+    Image hawaiianImage = new Image("file:hawaiian.jpg");
+    Image deluxeImage = new Image("file:deluxe.jpg");
+
+    //inject FXML objects
+    @FXML
+    private ComboBox pizzaComboBox;
+    @FXML
+    private ImageView imageBox;
+    @FXML
+    private ComboBox sizeComboBox;
+    @FXML
+    private ListView<String> toppingsList = new ListView<String>(toppings);
+    @FXML
+    private  Button addToppingButton;
+    @FXML
+    private Button removeToppingButton;
+    @FXML
+    private Button clearSelectButton;
+    @FXML
+    private ListView selectedToppingBox;
+    @FXML
+    private Button addToOrderButton;
+    @FXML
+    private Button showOrderButton;
+    @FXML
+    private TextArea outputArea;
+
+
+    /**
+     * initializes values and injects data
+     * @param location
+     * @param resource
+     */
+    public void initialize(URL location, ResourceBundle resource){
+        pizzaComboBox.setItems(types);
+        sizeComboBox.setItems(sizes);
+        pizzaComboBox.setValue("Build your own");
+        sizeComboBox.setValue("Medium");
+        toppingsList.setItems(toppings);
+        imageBox.setImage(byoImage);
+    }
+
+
+    /**
+     * disables boxes
+     */
+    public void pizzaComBoxselect(){
+
+        String selected = (String) pizzaComboBox.getSelectionModel().getSelectedItem();
+        if(selected.equals("Deluxe") ){
+            toppingsList.setDisable(true);
+            imageBox.setImage(deluxeImage);
+            return;
+        }
+        if(selected.equals("Hawaiian")){
+            toppingsList.setDisable(true);
+            imageBox.setImage(hawaiianImage);
+            return;
+        }
+        imageBox.setImage(byoImage);
+        toppingsList.setDisable(false);
+    }
+
+    public void addButtonClicked() throws IllegalArgumentException{
+
+    }
+
+    /**
+     * starts the show order screen
+     * @throws IOException
+     */
+    public void showOrderButtonPressed(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/scene2/Scene2.fxml"));
+        Controller orderController = loader.getController();
+        Parent root = loader.load();
+
+        Stage window = (Stage) showOrderButton.getScene().getWindow();
+        window.setScene(new Scene(root));
+        window.show();
+
+    }
+}
