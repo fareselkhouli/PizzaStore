6491678c17ccc677b5bfdaf1f90721b045fb365a
fareselkhouli <fareselkhouli@gmail.com>
2020-04-13 00:43:46 -0400

    fixed initialize

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,37 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
 import sample.pizza.*;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    ObservableList<String> toppings = FXCollections.observableArrayList(
+            "Beef", "Cheese", "Chicken", "Green Pepper", "Ham",
+            "Mushroom", "Onion", "Pepperoni", "Pineapple", "Sausage");
+    ObservableList<String> types = FXCollections.observableArrayList("Build your own",
+            "Deluxe",
+            "Hawaiian");
+    ObservableList<String> sizes = FXCollections.observableArrayList("Medium",
+            "Small",
+            "Large");
+
     //inject FXML objects
     @FXML
     private ComboBox pizzaComboBox;
     @FXML
     private ComboBox sizeComboBox;
     @FXML
-    private ListView toppingsList;
+    private ListView<String> toppingsList = new ListView<String>(toppings);
     @FXML
     private  Button addToppingButton;
     @FXML
@@ -30,32 +47,31 @@ public class Controller {
     @FXML
     private TextArea outputArea;
 
-    public Pizza cs213 = new Pizza();
+
     /**
-     * Disable all the wrong fields
+     * initializes values and injects data
+     * @param location
+     * @param resource
      */
-    pizzaComboBox.getItems().addAll(
-            "Build your own",
-                    "Deluxe",
-                    "Hawaiian"
-    );
-    sizeComboBox.getItems().addAll(
-            "Medium",
-            "Small",
-            "Large"
-    )
-    ListView<String> toppingsList = new ListView<String>();
-    ObservableList<String> items = FXCollections.observableArrayList(
-            "Beef", "Cheese", "Chicken", "Green Pepper", "Ham",
-            "Mushroom", "Onion", "Pepperoni", "Pineapple", "Sausage");
-    toppingList.setItems(items);
+    public void initialize(URL location, ResourceBundle resource){
+        pizzaComboBox.setItems(types);
+        sizeComboBox.setItems(sizes);
+        pizzaComboBox.setValue("Build your own");
+        sizeComboBox.setValue("Medium");
+        toppingsList.setItems(toppings);
+    }
+
+
+    /**
+     * disables boxes
+     */
     public void pizzaComBoxselect(){
-        if(pizzaComboBox=="Deluxe"){
-            toppingsList.setDisable(true);
-        } else if (pizzaComboBox=="Hawaiian"){
+        String selected = (String) pizzaComboBox.getSelectionModel().getSelectedItem();
+        if(selected.equals("Deluxe") || selected.equals("Hawaiian")){
             toppingsList.setDisable(true);
         }
     }
+    
     public void addButtonClicked() throws IllegalArgumentException{
         if()
     }
